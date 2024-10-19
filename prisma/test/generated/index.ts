e8a719966b98e26a17c8b8dcf2716b4c93a429ea
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-10-19 08:06:03 +0200

    fix: update dependencies

--- prisma/test/generated/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./interfaces";
-export * from "./models";
-export * from "./services/PrismaService";
-export * from "./repositories";
--- prisma/test/generated/repositories/index.ts
+++
@@ -1,2 +0,0 @@
-export { PostsRepository } from "./PostsRepository";
-export { UsersRepository } from "./UsersRepository";
